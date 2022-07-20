23f22ed5274140defbb966610cb1d0399e584cb0
samanthahaque <samantha.simona.xyz.007@gmail.com>
2022-07-20 21:54:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function why() {
+  location.href = "why.html";
+}
+function work() {
+  location.href = "work.html";
+}
+function team() {
+  location.href = "team.html";
+}
+function bd() {
+  location.href = "blog.html";
+}
